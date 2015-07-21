162445b01ea68881fc2e94a4cc053deb5ae52986
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2015-07-21 21:27:44 +0200

    Add a way to determine orientation from its short string representation

--- include/emp_types.h
+++ include/emp_types.h
@@ -20,6 +20,7 @@
 #ifndef EMP_TYPES_H
 #define EMP_TYPES_H
 
+#include "quicky_exception.h"
 #include <string>
 
 namespace edge_matching_puzzle
@@ -36,6 +37,7 @@ namespace edge_matching_puzzle
     inline static const std::string & kind2string(const t_kind & p_kind);
     inline static const std::string & orientation2string(const t_orientation & p_orientation);
     inline static const char & orientation2short_string(const t_orientation & p_orientation);
+    inline static const t_orientation short_string2orientation(const char & p_char);
   private:
     static const std::string m_kind_strings[((uint32_t)t_kind::CORNER) + 1];
     static const std::string m_orientation_strings[((uint32_t)t_orientation::WEST) + 1];
@@ -60,6 +62,27 @@ namespace edge_matching_puzzle
       return m_short_orientation_strings[(uint32_t) p_orientation];
     }
 
+  //----------------------------------------------------------------------------
+  const emp_types::t_orientation emp_types::short_string2orientation(const char & p_char)
+  {
+    switch(p_char)
+      {
+      case 'N':
+        return emp_types::t_orientation::NORTH;
+        break;
+      case 'E':
+        return emp_types::t_orientation::EAST;
+        break;
+      case 'S':
+        return emp_types::t_orientation::SOUTH;
+        break;
+      case 'W':
+        return emp_types::t_orientation::WEST;
+        break;
+      default:
+        throw quicky_exception::quicky_logic_exception("Unkown short string orientation '" + std::string(1,p_char) +"'",__LINE__,__FILE__);
+      }
+  }
 }
 #endif //EMP_TYPES_H
 //EOF
