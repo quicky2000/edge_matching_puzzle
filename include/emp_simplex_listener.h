f82fe95e6841389f4c107b2fe805b5555eb2ccaa
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2019-03-12 13:14:59 +0100

    Modify emp_simplex_listener to support all solves implementing simplex_listener_target_if interface

--- include/emp_simplex_listener.h
+++ include/emp_simplex_listener.h
@@ -20,23 +20,18 @@
 #include "simplex_array.h"
 #include <iostream>
 #include "simplex_variable.h"
+#include "simplex_listener_target_if.h"
 #include <vector>
-namespace simplex
-{
-  template <typename COEF_TYPE, typename ARRAY_TYPE>
-    class simplex_solver_base;
-}
-
 
 namespace edge_matching_puzzle
 {
-  template <typename COEF_TYPE,typename ARRAY_TYPE=simplex::simplex_array<COEF_TYPE>>
+  template <typename COEF_TYPE>
   class emp_simplex_listener
   {
   public:
-    inline emp_simplex_listener(const simplex::simplex_solver_base<COEF_TYPE,ARRAY_TYPE> & p_simplex,
-                                const std::vector<simplex_variable*> & p_variables,
-                                std::ostream & p_ostream = std::cout
+    inline emp_simplex_listener(const simplex::simplex_listener_target_if<COEF_TYPE> & p_simplex
+                               ,const std::vector<simplex_variable*> & p_variables
+                               ,std::ostream & p_ostream = std::cout
                                );
     inline void start_iteration(const unsigned int & p_nb_iteration);
     inline void new_input_var_event(const unsigned int & p_input_variable_index);
@@ -44,17 +39,17 @@ namespace edge_matching_puzzle
     inline void new_Z0(COEF_TYPE p_z0);
     private:
       unsigned int m_nb_iteration;
-      const simplex::simplex_solver_base<COEF_TYPE,ARRAY_TYPE> & m_simplex;
+      const simplex::simplex_listener_target_if<COEF_TYPE> & m_simplex;
       std::ostream & m_ostream;
       const std::vector<simplex_variable*> & m_variables;
   };
 
   //----------------------------------------------------------------------------
-  template <typename COEF_TYPE,typename ARRAY_TYPE>
-    emp_simplex_listener<COEF_TYPE,ARRAY_TYPE>::emp_simplex_listener(const simplex::simplex_solver_base<COEF_TYPE,ARRAY_TYPE> & p_simplex,
-                                                                     const std::vector<simplex_variable*> & p_variables,
-                                                                     std::ostream & p_ostream
-                                                                    ):
+  template <typename COEF_TYPE>
+  emp_simplex_listener<COEF_TYPE>::emp_simplex_listener(const simplex::simplex_listener_target_if<COEF_TYPE> & p_simplex
+                                                       ,const std::vector<simplex_variable*> & p_variables
+                                                       ,std::ostream & p_ostream
+                                                       ):
             m_nb_iteration(0),
             m_simplex(p_simplex),
             m_ostream(p_ostream),
@@ -63,16 +58,15 @@ namespace edge_matching_puzzle
   }
 
   //----------------------------------------------------------------------------
-  template <typename COEF_TYPE,typename ARRAY_TYPE>
-  void emp_simplex_listener<COEF_TYPE,ARRAY_TYPE>::start_iteration(const unsigned int & p_nb_iteration)
+  template <typename COEF_TYPE>
+  void emp_simplex_listener<COEF_TYPE>::start_iteration(const unsigned int & p_nb_iteration)
   {
     m_nb_iteration = p_nb_iteration;
   }
  
   //----------------------------------------------------------------------------
-  template <typename COEF_TYPE,typename ARRAY_TYPE>
-  void emp_simplex_listener<COEF_TYPE,ARRAY_TYPE>::new_input_var_event(const unsigned int & p_input_variable_index
-					     )
+  template <typename COEF_TYPE>
+  void emp_simplex_listener<COEF_TYPE>::new_input_var_event(const unsigned int & p_input_variable_index)
   {
       m_ostream << "Iteration[" << m_nb_iteration << "] : New input variable selected : " << p_input_variable_index;
       if(p_input_variable_index < m_variables.size())
@@ -83,9 +77,8 @@ namespace edge_matching_puzzle
   }
   
   //----------------------------------------------------------------------------
-  template <typename COEF_TYPE,typename ARRAY_TYPE>
-  void emp_simplex_listener<COEF_TYPE,ARRAY_TYPE>::new_output_var_event(const unsigned int & p_output_variable_index
-					     )
+  template <typename COEF_TYPE>
+  void emp_simplex_listener<COEF_TYPE>::new_output_var_event(const unsigned int & p_output_variable_index)
   {
       //assert(p_output_variable_index < m_variables.size());
       m_ostream << "Iteration[" << m_nb_iteration << "] : New output variable selected : " << p_output_variable_index;
@@ -97,11 +90,11 @@ namespace edge_matching_puzzle
   }
 
   //----------------------------------------------------------------------------
-  template<typename COEF_TYPE,typename ARRAY_TYPE>
-  void emp_simplex_listener<COEF_TYPE,ARRAY_TYPE>::new_Z0(const COEF_TYPE p_z0)
+  template<typename COEF_TYPE>
+  void emp_simplex_listener<COEF_TYPE>::new_Z0(const COEF_TYPE p_z0)
   {
-    m_ostream << "Iteration[" << m_nb_iteration << "] : New Z0 : " << p_z0 << std::endl;
-    //m_simplex.display_array(m_ostream);
+      m_ostream << "Iteration[" << m_nb_iteration << "] : New Z0 : " << p_z0 << std::endl;
+      //m_simplex.display_array(m_ostream);
   }
 
 }
