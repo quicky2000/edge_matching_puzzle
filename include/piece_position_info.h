f765869250556938ec10a5b6856d5f3abea0ca59
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2020-11-24 15:28:53 +0100

    Add modifiers and constructor do deal with bit values in piece_position_info

--- include/piece_position_info.h
+++ include/piece_position_info.h
@@ -17,12 +17,14 @@
 */
 
 #ifndef EMP_PIECE_POSITION_INFO_H
-#define EMP_ETERNITY2_PIECE_POSITION_INFO_H
+#define EMP_PIECE_POSITION_INFO_H
 
+#include "emp_types.h"
 #include <array>
 #include <cassert>
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 
 namespace edge_matching_puzzle
 {
@@ -42,16 +44,45 @@ namespace edge_matching_puzzle
      * | W0 .. W7 || W0 .. W7 || W0 .. W7 || W0 .. W7 |
      */
     class piece_position_info
+#ifdef __NVCC__
+    : public CUDA_memory_managed_item
+#endif // __NVCC__
     {
         friend
         std::ostream & operator<<(std::ostream & p_stream, const piece_position_info & p_info);
 
       public:
+
         inline
         piece_position_info();
 
+        inline explicit
+        piece_position_info(uint32_t p_value);
+
         piece_position_info(const piece_position_info & ) = default;
         piece_position_info & operator=(const piece_position_info &) = default;
+
+        inline
+        void clear_bit(unsigned int p_bit_index);
+
+        inline
+        void clear_bit( unsigned int p_id
+                      , emp_types::t_orientation p_orientation
+                      );
+
+        inline
+        void set_bit(unsigned int p_bit_index);
+
+        inline
+        void set_bit( unsigned int p_index
+                    , emp_types::t_orientation p_orientation
+                    );
+
+        inline static
+        void set_init_value(uint32_t p_value);
+
+        inline
+        void clear();
 
         /**
          * Word access for CUDA warp operations
@@ -87,20 +118,42 @@ namespace edge_matching_puzzle
         bool operator==(const piece_position_info &) const;
 
       private:
+
+        inline
+        void clear_bit( unsigned int p_word_index
+                      , unsigned int p_bit_index
+                      );
+
+        inline
+        void set_bit( unsigned int p_word_index
+                    , unsigned int p_bit_index
+                    );
+
 #ifndef __NVCC__
         std::array<uint32_t, 32> m_info;
 #else // __NVCC__
         uint32_t m_info[32];
 #endif // __NVCC__
 
+        // To do : replace by inline variable ( need to have recent CMake for CUDA c++17
+        static
+        uint32_t s_init_value;
+
     };
 
     //-------------------------------------------------------------------------
     piece_position_info::piece_position_info()
-    : m_info{ 0 ,0 , 0, 0, 0, 0, 0, 0
-            , 0 ,0 , 0, 0, 0, 0, 0, 0
-            , 0 ,0 , 0, 0, 0, 0, 0, 0
-            , 0 ,0 , 0, 0, 0, 0, 0, 0
+            : piece_position_info(s_init_value)
+    {
+
+    }
+
+    //-------------------------------------------------------------------------
+    piece_position_info::piece_position_info( uint32_t p_value)
+    : m_info{ p_value ,p_value , p_value, p_value, p_value, p_value, p_value, p_value
+            , p_value ,p_value , p_value, p_value, p_value, p_value, p_value, p_value
+            , p_value ,p_value , p_value, p_value, p_value, p_value, p_value, p_value
+            , p_value ,p_value , p_value, p_value, p_value, p_value, p_value, p_value
             }
     {
 
@@ -168,6 +221,64 @@ namespace edge_matching_puzzle
     }
 
     //-------------------------------------------------------------------------
+    void
+    piece_position_info::clear_bit(unsigned int p_word_index,
+                                   unsigned int p_bit_index
+                                  )
+    {
+        assert(p_word_index < 32);
+        assert(p_bit_index < 32);
+        m_info[p_word_index] &= ~(1u << p_bit_index);
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    piece_position_info::clear_bit(unsigned int p_bit_index)
+    {
+        assert(p_bit_index < 1024);
+        clear_bit(p_bit_index / 32, p_bit_index % 32);
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    piece_position_info::clear_bit( unsigned int p_id
+                                  , emp_types::t_orientation p_orientation
+                                  )
+    {
+        assert(p_id < 256);
+        clear_bit(8 * static_cast<unsigned int>(p_orientation) + p_id / 32, p_id % 32);
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    piece_position_info::set_bit( unsigned int p_word_index
+                                , unsigned int p_bit_index
+                                )
+    {
+        assert(p_word_index < 32);
+        assert(p_bit_index < 32);
+        m_info[p_word_index] |= (1u << p_bit_index);
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    piece_position_info::set_bit(unsigned int p_bit_index)
+    {
+        assert(p_bit_index < 1024);
+        set_bit(p_bit_index / 32, p_bit_index % 32);
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    piece_position_info::set_bit( unsigned int p_index
+                                , emp_types::t_orientation p_orientation
+                                )
+    {
+        assert(p_index < 256);
+        set_bit(8 * static_cast<unsigned int>(p_orientation) + p_index / 32, p_index % 32);
+    }
+
+    //-------------------------------------------------------------------------
     inline
     std::ostream & operator<<(std::ostream & p_stream, const piece_position_info & p_info)
     {
@@ -181,6 +292,28 @@ namespace edge_matching_puzzle
         }
         return p_stream;
     }
+
+    //-------------------------------------------------------------------------
+    void
+    piece_position_info::set_init_value(uint32_t p_value)
+    {
+        s_init_value = p_value;
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    piece_position_info::clear()
+    {
+#ifndef __NVCC__
+        std::transform(m_info.begin(), m_info.end(), m_info.begin(), [](uint32_t p_item){return 0x0;});
+#else // __NVCC__
+        for(unsigned int l_index = 0; l_index < 32; ++l_index)
+        {
+            m_info[l_index] = 0;
+        }
+#endif // __NVCC__
+    }
+
 }
 #endif //EMP_PIECE_POSITION_INFO_H
 // EOF
---
+++ src/piece_position_info.cpp
@@ -0,0 +1,24 @@
+/*
+      This file is part of edge_matching_puzzle
+      Copyright (C) 2020  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include "piece_position_info.h"
+
+namespace edge_matching_puzzle
+{
+    uint32_t piece_position_info::s_init_value = 0;
+}
