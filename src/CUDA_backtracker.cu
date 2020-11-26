7b515020cd5398c0e0416ba48269b63efd480d66
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2020-11-26 17:52:23 +0100

    Add some check on CUDA device properties

--- src/CUDA_backtracker.cu
+++ src/CUDA_backtracker.cu
@@ -21,6 +21,9 @@
 #include "my_cuda.h"
 #include "transition_manager.h"
 #include <chrono>
+#ifdef __NVCC__
+#include "thrust/version.h"
+#endif // __NVCC__
 
 namespace edge_matching_puzzle
 {
@@ -177,6 +180,38 @@ namespace edge_matching_puzzle
                         , const emp_strategy_generator & p_strategy_generator
                         )
     {
+        std::cout << "CUDA version  : " << CUDART_VERSION << std::endl;
+        std::cout << "THRUST version: " << THRUST_MAJOR_VERSION << "." << THRUST_MINOR_VERSION << "." << THRUST_SUBMINOR_VERSION << std::endl;
+
+        int l_cuda_device_nb = 0;
+        cudaError_t l_cuda_status = cudaGetDeviceCount(&l_cuda_device_nb);
+        if(cudaSuccess != l_cuda_status)
+        {
+            throw quicky_exception::quicky_runtime_exception(cudaGetErrorString(l_cuda_status), __LINE__, __FILE__);
+        }
+        std::cout << "Number of CUDA devices: " << l_cuda_device_nb << std::endl;
+
+        for(int l_device_index = 0; l_device_index < l_cuda_device_nb; ++l_device_index)
+        {
+            std::cout << "Cuda device[" << l_device_index << "]" << std::endl;
+            cudaDeviceProp l_properties;
+            cudaGetDeviceProperties(&l_properties, l_device_index);
+            std::cout << R"(\tName                      : ")" << l_properties.name << R"(")" << std::endl;
+            std::cout <<   "\tDevice compute capability : " << l_properties.major << "." << l_properties.minor << std::endl;
+            std::cout <<    "\tWarp size                 : " << l_properties.warpSize << std::endl;
+            if(l_properties.warpSize != 32)
+            {
+                throw quicky_exception::quicky_logic_exception("Unsupported warp size" + std::to_string(l_properties.warpSize), __LINE__, __FILE__);
+            }
+            std::cout <<    "\tMultiprocessor count      : " << l_properties.multiProcessorCount << std::endl;
+            std::cout <<    "\tManaged Memory            : " << l_properties.managedMemory << std::endl;
+            if(!l_properties.managedMemory)
+            {
+                throw quicky_exception::quicky_logic_exception("Managed memory is not supported", __LINE__, __FILE__);
+            }
+            std::cout << std::endl;
+        }
+
         unsigned int l_nb_stack = 1;
         CUDA_backtracker_stack<NB_PIECES> * l_stacks;
         const transition_manager<NB_PIECES> * l_transition_manager;
