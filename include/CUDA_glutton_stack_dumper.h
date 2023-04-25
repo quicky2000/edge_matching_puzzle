20623cf891df426b729981e84e0b9287baa2802e
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2023-04-25 13:02:42 +0200

    Add methods to load CUDA_glutton_stack from XML dump

--- include/CUDA_glutton_stack_dumper.h
+++ include/CUDA_glutton_stack_dumper.h
@@ -41,6 +41,10 @@ namespace edge_matching_puzzle
         void
         dump(const CUDA_glutton_max_stack & p_stack);
 
+        inline
+        void
+        load(CUDA_glutton_max_stack & p_stack);
+
       private:
 
         inline static
@@ -101,6 +105,12 @@ namespace edge_matching_puzzle
                            ,XMLNode & p_node
                            );
 
+        inline
+        void
+        load_position_infos(CUDA_glutton_max_stack & p_stack
+                           ,const XMLNode & p_node
+                           );
+
         inline static
         void
         dump_level(uint32_t p_level
@@ -108,11 +118,25 @@ namespace edge_matching_puzzle
                   ,XMLNode & p_node
                   );
 
+        inline
+        void
+        load_level(uint32_t p_level
+                  ,CUDA_glutton_max_stack & p_stack
+                  ,const XMLNode & p_node
+                  );
+
         inline static
         void
         dump(uint32_t p_index
             ,const CUDA_piece_position_info2 & p_position_info
             ,XMLNode & p_node
+            );
+
+        inline
+        void
+        load(uint32_t p_index
+            ,CUDA_piece_position_info2 & p_position_info
+            ,const XMLNode & p_node
             );
 
         template<typename T>
@@ -124,6 +148,28 @@ namespace edge_matching_puzzle
                   ,XMLNode & p_node
                   );
 
+        template<typename T>
+        inline
+        void
+        load_array(const std::string & p_name
+                  ,uint32_t p_size
+                  ,T & p_info_to_position
+                  ,const XMLNode & p_node
+                  );
+
+        inline
+        void
+        check(const std::string & p_attribute_name
+             ,uint32_t p_value
+             ,const XMLNode & p_node
+             );
+
+        inline
+        uint32_t
+        extract_attribute(const std::string & p_name
+                         ,const XMLNode & p_node
+                         );
+
         std::string m_name;
     };
 
@@ -132,6 +178,44 @@ namespace edge_matching_puzzle
     :m_name{std::move(p_name)}
     {
 
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    CUDA_glutton_stack_dumper::load(CUDA_glutton_max_stack & p_stack)
+    {
+        XMLResults l_result{eXMLErrorNone,0,0};
+        XMLNode l_root_node = XMLNode::parseFile(m_name.c_str(), "CUDA_glutton_stack", &l_result);
+
+        if(eXMLErrorNone != l_result.error)
+        {
+            if(eXMLErrorFileNotFound == l_result.error)
+            {
+                throw quicky_exception::quicky_runtime_exception( "File \"" + m_name + "\" not found"
+                                                                , __LINE__
+                                                                , __FILE__
+                                                                );
+            }
+            else
+            {
+                std::string l_error_msg = XMLNode::getError(l_result.error);
+                throw quicky_exception::quicky_logic_exception( "\"" + l_error_msg + "\" at line " + std::to_string(l_result.nLine) + " and column " + std::to_string(l_result.nColumn) + " of file \"" + m_name + "\""
+                                                              , __LINE__
+                                                              , __FILE__
+                                                              );
+            }
+        }
+
+        check("size", static_cast<uint32_t>(p_stack.get_size()), l_root_node);
+        check("nb_pieces", static_cast<uint32_t>(p_stack.get_nb_pieces()), l_root_node);
+        p_stack.m_level = extract_attribute("level", l_root_node);
+        p_stack.m_max = extract_attribute("max", l_root_node);
+        load_array("info_to_position", p_stack.get_size(), p_stack.m_info_index_to_position_index, l_root_node);
+        load_array("position_to_info", static_cast<uint32_t>(p_stack.get_nb_pieces()), p_stack.m_position_index_to_info_index, l_root_node);
+        load_array("played_info", p_stack.get_size(), p_stack.m_played_info, l_root_node);
+        load_array("max_played_info", p_stack.get_max(), p_stack.m_max_played_info, l_root_node);
+        load_array("available_pieces", 8, p_stack.m_available_pieces, l_root_node);
+        load_position_infos(p_stack, l_root_node);
     }
 
     //-------------------------------------------------------------------------
@@ -153,6 +237,35 @@ namespace edge_matching_puzzle
     }
 
     //-------------------------------------------------------------------------
+    uint32_t
+    CUDA_glutton_stack_dumper::extract_attribute(const std::string & p_name
+                                                ,const XMLNode & p_node
+                                                )
+    {
+        XMLCSTR l_value_char_ptr = p_node.getAttribute(p_name.c_str());
+        if(nullptr == l_value_char_ptr)
+        {
+            throw quicky_exception::quicky_logic_exception("Missing size attribute in " + std::string(p_node.getName()) + " node of file '" + m_name + "'", __LINE__, __FILE__);
+        }
+        uint32_t l_value = static_cast<uint32_t>(std::stoul(l_value_char_ptr));
+        return l_value;
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    CUDA_glutton_stack_dumper::check(const std::string & p_attribute_name
+                                    ,uint32_t p_value
+                                    ,const XMLNode & p_node
+                                    )
+    {
+        uint32_t l_value = extract_attribute(p_attribute_name, p_node);
+        if (l_value != p_value)
+        {
+            throw quicky_exception::quicky_logic_exception(p_attribute_name + " from XML (" + std::to_string(l_value) + ") is different from " + p_attribute_name + " of stack (" + std::to_string(p_value) +")", __LINE__, __FILE__);
+        }
+    }
+
+    //-------------------------------------------------------------------------
     void
     CUDA_glutton_stack_dumper::dump_position_infos(const CUDA_glutton_max_stack & p_stack
                                                   ,XMLNode & p_node
@@ -167,6 +280,24 @@ namespace edge_matching_puzzle
 
     //-------------------------------------------------------------------------
     void
+    CUDA_glutton_stack_dumper::load_position_infos(CUDA_glutton_max_stack & p_stack
+                                                  ,const XMLNode & p_node
+                                                  )
+    {
+        XMLNode l_node = p_node.getChildNode("position_infos");
+        if (l_node.isEmpty())
+        {
+            throw quicky_exception::quicky_logic_exception("Node position infos in file \"" + m_name + "\" is empty", __LINE__, __FILE__);
+        }
+        for(uint32_t l_index = 0; l_index < p_stack.get_size(); ++l_index)
+        {
+            load_level(l_index, p_stack, l_node);
+        }
+    }
+
+
+    //-------------------------------------------------------------------------
+    void
     CUDA_glutton_stack_dumper::dump_level(uint32_t p_level
                                          ,const CUDA_glutton_max_stack & p_stack
                                          ,XMLNode & p_node
@@ -177,6 +308,25 @@ namespace edge_matching_puzzle
         for(uint32_t l_index = 0; l_index < p_stack.get_size() - p_level; ++l_index)
         {
             dump(l_index, p_stack.get_position_info(p_level, static_cast<info_index_t>(l_index)), l_node);
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    CUDA_glutton_stack_dumper::load_level(uint32_t p_level
+                                         ,CUDA_glutton_max_stack & p_stack
+                                         ,const XMLNode & p_node
+                                         )
+    {
+        XMLNode l_node = p_node.getChildNode("level", p_level);
+        if (l_node.isEmpty())
+        {
+            throw quicky_exception::quicky_logic_exception("Node level[" + std::to_string(p_level) + " in file \"" + m_name + "\" is empty", __LINE__, __FILE__);
+        }
+        check("index", p_level, l_node);
+        for(uint32_t l_index = 0; l_index < p_stack.get_size() - p_level; ++l_index)
+        {
+            load(l_index, p_stack.get_position_info(p_level, static_cast<info_index_t>(l_index)), l_node);
         }
     }
 
@@ -198,6 +348,39 @@ namespace edge_matching_puzzle
             l_item_node.addAttribute("index", std::to_string(l_index).c_str());
         }
     }
+
+    //-------------------------------------------------------------------------
+    void
+    CUDA_glutton_stack_dumper::load(uint32_t p_index
+                                   ,CUDA_piece_position_info2 & p_position_info
+                                   ,const XMLNode & p_node
+                                   )
+    {
+        XMLNode l_node = p_node.getChildNode("position_info", p_index);
+        if (l_node.isEmpty())
+        {
+            throw quicky_exception::quicky_logic_exception("Node position_info[" + std::to_string(p_index) + " in file \"" + m_name + "\" is empty", __LINE__, __FILE__);
+        }
+        check("index", p_index, l_node);
+        for(uint32_t l_index = 0; l_index < 32; ++l_index)
+        {
+            XMLNode l_item_node = l_node.getChildNode("word", l_index);
+            if (l_item_node.isEmpty())
+            {
+                throw quicky_exception::quicky_logic_exception("Node word[" + std::to_string(l_index) + "] in position_info[" + std::to_string(p_index) + " in file \"" + m_name + "\" is empty", __LINE__, __FILE__);
+            }
+            check("index", l_index, l_item_node);
+            XMLCSTR l_value_char_ptr = l_item_node.getText();
+            if(nullptr == l_value_char_ptr)
+            {
+                throw quicky_exception::quicky_logic_exception("Missing word text in " + std::string(p_node.getName()) + " node of file '" + m_name + "'", __LINE__, __FILE__);
+            }
+            uint32_t l_value = static_cast<uint32_t>(std::stoul(l_value_char_ptr, nullptr, 0));
+            p_position_info.set_word(l_index, l_value);
+        }
+
+    }
+
 
     //-------------------------------------------------------------------------
     void
@@ -296,6 +479,39 @@ namespace edge_matching_puzzle
         }
     }
 
+    //-------------------------------------------------------------------------
+    template<typename T>
+    void
+    CUDA_glutton_stack_dumper::load_array(const std::string & p_name
+                                         ,uint32_t p_size
+                                         ,T & p_info_to_position
+                                         ,const XMLNode & p_node
+                                         )
+    {
+        const XMLNode l_array_node = p_node.getChildNode(p_name.c_str());
+        if(l_array_node.isEmpty())
+        {
+            throw quicky_exception::quicky_logic_exception("No node \"" + p_name + "\" in node \"" + p_node.getName() + "\" from file \"" + m_name + "\"", __LINE__, __FILE__);
+        }
+
+        check("size", p_size, l_array_node);
+        for(uint32_t l_index = 0; l_index < p_size; ++l_index)
+        {
+            XMLNode l_item_node = l_array_node.getChildNode(l_index);
+            if(l_item_node.isEmpty())
+            {
+                throw quicky_exception::quicky_logic_exception("No node for item " + std::to_string(l_index) + " in node \"" + p_name + "\" from file \"" + m_name + "\"", __LINE__, __FILE__);
+            }
+            check("index", l_index, l_item_node);
+            XMLCSTR l_value_char_ptr = l_item_node.getText();
+            if (nullptr == l_value_char_ptr)
+            {
+                throw quicky_exception::quicky_logic_exception("Empty test in node item[" + std::to_string(l_index) + "]  in node \"" + p_name + "\" from file \"" + m_name + "\"", __LINE__, __FILE__);
+            }
+            uint64_t l_value = static_cast<uint32_t >(std::stoul(l_value_char_ptr,nullptr, 0));
+            p_info_to_position[l_index] = l_value;
+        }
+    }
 }
 #endif //EDGE_MATCHING_PUZZLE_CUDA_GLUTTON_STACK_DUMPER_H
 // EOF
